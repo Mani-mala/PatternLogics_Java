79c7830675615069ea9707dc9d2f0a65f555e673
Mani-mala <manimalaece99@gmail.com>
2025-03-24 21:34:04 +0530

    Add files via upload

---
+++ PatternDesign.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PatternDesign {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int num=sc.nextInt();
+        int cur=0,temp=0;
+        for(int i=1;i<=num;i++)
+        {
+            System.out.print(i+" ");
+            cur=i;
+            temp=num-1;
+           for(int j=1;j<i;j++)
+           {
+            cur+=temp--;
+            System.out.print(cur+" ");
+           }
+           cur=0;
+           System.out.println();
+        }
+        sc.close();
+    }
+}
