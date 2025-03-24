0ee612079872fe51d0c52a4e1479260c57149bd5
Mani-mala <manimalaece99@gmail.com>
2025-03-24 20:21:11 +0530

    Update PatternFromInsta2.java

--- PatternFromInsta2.java
+++ PatternFromInsta2.java
@@ -5,25 +5,29 @@ public class PatternFromInsta2 {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the number: ");
         int num=sc.nextInt();
-        int p=1,col=num,row;
+        int p=1,col=num,row,current=0;                 
         System.out.println("1");
         for(int i=2;i<=num;i++)
         {
-            row=1;
-            for(int j=1;j<=i;j++)
+            row=0;
+            p+=col--;
+            System.out.print(p+" ");
+            current=p;
+            for(int j=1;j<i;j++)
             {
-                if(j==1)
-                {
-                    p+=col--;
-                    System.out.print(p+" ");
-                   
-                }
-                else{
-                    System.out.print(p-col+row+" ");
-                    row++;
-                }
+                current-=(col+row);
+                System.out.print(current+" ");
+                row++;
             }
             System.out.println();
         }
+      
+        }
     }
-}
+/* output  
+    1
+    7 2
+    12 8 3
+    16 13 9 4
+    19 17 14 10 5
+    21 20 18 15 11 6  */
