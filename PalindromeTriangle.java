ab5ad06ef74d89e00785bbab5bd9aec309557ef3
Mani-mala <manimalaece99@gmail.com>
2025-04-01 12:48:54 +0530

    Update PalindromeTriangle.java

--- PalindromeTriangle.java
+++ PalindromeTriangle.java
@@ -7,7 +7,7 @@ public class PalindromeTriangle {
 		int n=sc.nextInt();
 		for(int i=1;i<=n;i++)
 		{
-            int p=i;
+            		int p=i;
 			for(int j=1;j<=2*(n-i);j++)
 			{
 				System.out.print(" ");
@@ -16,8 +16,8 @@ public class PalindromeTriangle {
 			{
 				System.out.print(p-- +" ");
 			}
-            p+=2;
-            for(int j=1;j<i;j++)
+            		p+=2;
+            		for(int j=1;j<i;j++)
 			{
 				System.out.print(p++ +" ");
 			}
